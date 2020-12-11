0c0017ec8e90e43ecc2ff19be4c7e44c5973f5ed
nlevy <nicolaspierrelevy@gmail.com>
2020-12-11 09:54:48 +0100

    Handle wfd::DialogError::UnsupportedFilepath

--- src/impl/mod.rs
+++ src/impl/mod.rs
@@ -1,7 +1,12 @@
 #[cfg(target_os = "macos")]
 pub(crate) mod mac;
 
-#[cfg(all(unix, not(target_os = "macos"), not(target_os = "ios"), not(target_os = "android")))]
+#[cfg(all(
+    unix,
+    not(target_os = "macos"),
+    not(target_os = "ios"),
+    not(target_os = "android")
+))]
 pub(crate) mod gnu;
 
 #[cfg(target_os = "windows")]
