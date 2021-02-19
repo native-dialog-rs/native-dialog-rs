bc8e35873c93c7c6764acea3e1c9356d321b56b7
Balthild <ibalthild@gmail.com>
2021-02-19 15:40:37 +0800

    Change the dependency `dirs` to `dirs-next`

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,6 @@
 #[cfg(not(target_os = "macos"))]
 mod resolve_tilde {
+    use dirs_next::home_dir;
     use std::path::{Component, Path, PathBuf};
 
     pub fn resolve_tilde<P: AsRef<Path> + ?Sized>(path: &P) -> Option<PathBuf> {
@@ -7,7 +8,7 @@ mod resolve_tilde {
 
         let mut components = path.as_ref().components();
         match components.next() {
-            Some(Component::Normal(c)) if c == "~" => result.push(dirs::home_dir()?),
+            Some(Component::Normal(c)) if c == "~" => result.push(home_dir()?),
             Some(c) => result.push(c),
             None => {}
         };
