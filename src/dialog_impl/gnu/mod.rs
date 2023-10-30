e73cbead5c52e17d8d0d7bde7149d27e874aa84c
Balthild <ibalthild@gmail.com>
2023-10-30 17:49:20 -0500

    Some refactor for Linux/BSD impl

--- src/dialog_impl/gnu/mod.rs
+++ src/dialog_impl/gnu/mod.rs
@@ -1,6 +1,7 @@
 use crate::Error;
-use std::env;
+use ascii::AsAsciiStr;
 use std::process::Command;
+use versions::SemVer;
 
 mod file;
 pub(crate) mod message;
@@ -11,30 +12,54 @@ enum UseCommand {
 }
 
 fn should_use() -> Option<UseCommand> {
-    let has_display = match env::var("DISPLAY") {
+    let has_display = match std::env::var("DISPLAY") {
         Ok(display) => !display.is_empty(),
         _ => false,
     };
+    if !has_display {
+        return None;
+    }
 
-    if has_display {
-        // Prefer KDialog if the user is logged into a KDE session
-        let kdialog_available = which::which("kdialog").is_ok();
+    let candidates = match std::env::var("XDG_CURRENT_DESKTOP").as_deref() {
+        Ok("KDE") => [use_kdialog, use_zenity],
+        _ => [use_zenity, use_kdialog],
+    };
 
-        if let Ok(desktop) = env::var("XDG_CURRENT_DESKTOP") {
-            if kdialog_available && desktop == "KDE" {
-                return Some(UseCommand::KDialog(Command::new("kdialog")));
-            }
-        }
-
-        // Prefer Zenity otherwise
-        if which::which("zenity").is_ok() {
-            return Some(UseCommand::Zenity(Command::new("zenity")));
-        }
-
-        if kdialog_available {
-            return Some(UseCommand::KDialog(Command::new("kdialog")));
+    for candidate in candidates {
+        if let Some(command) = candidate() {
+            return Some(command);
         }
     }
 
     None
+}
+
+fn use_kdialog() -> Option<UseCommand> {
+    let path = which::which("kdialog").ok()?;
+    let command = Command::new(path);
+    Some(UseCommand::KDialog(command))
+}
+
+fn use_zenity() -> Option<UseCommand> {
+    let path = which::which("zenity").ok()?;
+    let command = Command::new(path);
+    Some(UseCommand::Zenity(command))
+}
+
+fn get_kdialog_version() -> Option<SemVer> {
+    get_version_output("kdialog")
+        .as_deref()
+        .and_then(|s| s.split_whitespace().last())
+        .and_then(SemVer::new)
+}
+
+fn get_zenity_version() -> Option<SemVer> {
+    get_version_output("zenity")
+        .as_deref()
+        .and_then(SemVer::new)
+}
+
+fn get_version_output(program: &str) -> Option<String> {
+    let output = Command::new(program).arg("--version").output().ok()?;
+    Some(output.stdout.as_ascii_str().ok()?.to_string())
 }
--- src/tests.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(target_os = "linux")]
-#[test]
-fn test_kdialog_version() {
-    use crate::dialog_impl::gnu::message;
-    let version = message::get_kdialog_version().unwrap();
-    println!("{}, {}, {}", version.0, version.1, version.2);
-}
