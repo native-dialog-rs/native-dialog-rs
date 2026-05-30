3ca21410e76a8baf0d53baa57562ef9e66c6f407
Balthild <ibalthild@gmail.com>
2026-05-30 12:19:41 -0500

    Replace `dirs` with `home`

--- src/utils/tilde.rs
+++ src/utils/tilde.rs
@@ -1,6 +1,6 @@
 use std::path::{Component, Path, PathBuf};
 
-use dirs::home_dir;
+use home::home_dir;
 
 pub fn resolve_tilde<P: AsRef<Path> + ?Sized>(path: &P) -> Option<PathBuf> {
     let mut result = PathBuf::new();
